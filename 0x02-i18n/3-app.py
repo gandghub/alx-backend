89f2001d1ea21f5e1e8d84fbd1d388402106da0c
gandghub <hackthehacker33@gmail.com>
2024-09-10 11:15:53 +0000

    0x02-i18n

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -4,26 +4,25 @@ Flask app with Babel and template localization.
 """
 
 from flask import Flask, render_template, request
-from flask_babel import Babel
+from flask_babel import Babel, _
 
 
 class Config:
-    """Config class for Flask app."""
-
-    DEBUG = True
+    """
+    Config class for Flask app.
+    """
     LANGUAGES = ["en", "fr"]
-    BABEL_DEFAULT_LOCALE = "en"
-    BABEL_DEFAULT_TIMEZONE = "UTC"
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
 
 
 app = Flask(__name__)
 app.config.from_object(Config)
-app.url_map.strict_slashes = False
 babel = Babel(app)
 
 
 @babel.localeselector
-def get_locale() -> str:
+def get_locale():
     """
     Determines the best match for supported languages from request headers.
     """
@@ -31,14 +30,12 @@ def get_locale() -> str:
 
 
 @app.route('/')
-def index() -> str:
-    '''default route
-
-    Returns:
-        html: homepage
-    '''
-    return render_template("3-index.html")
+def index():
+    """
+    Index route that renders a localized HTML page.
+    """
+    return render_template('3-index.html')
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run()
